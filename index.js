3b0edca488e6053ea5c2a90e4857aa18ed27c563
simshengqin <sqsim.2018@sis.smu.edu.sg>
2023-07-11 17:28:45 +0800

    Add XSS and SQLI json

---
+++ index.js
@@ -0,0 +1,12 @@
+const xss_latest = require('./latest/REQUEST-941-APPLICATION-ATTACK-XSS.json');
+const sqli_latest = require('./latest/REQUEST-942-APPLICATION-ATTACK-SQLI.json');
+const xss_v3_0_0 = require('./latest/REQUEST-941-APPLICATION-ATTACK-XSS.json');
+const sqli_v_3_0_0 = require('./latest/REQUEST-942-APPLICATION-ATTACK-SQLI.json');
+// Add more JSON file imports as needed
+
+module.exports = {
+    xss_latest,
+    sqli_latest,
+    xss_v3_0_0,
+    sqli_v_3_0_0
+};
